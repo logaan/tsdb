33acb337308d28d62cf5aff2daaf40798b94955a
Logan Campbell <colin@logaan.net>
2020-01-17 01:57:05 +1100

    matcher indexer and finder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,59 @@
-const world = '🗺️';
-
-export function hello(word: string = world): string {
-  return `Hello ${world}! `;
+interface Matcher<R> {
+	doesMatch(row: R): boolean;
 }
+
+interface Finder<K, R> {
+	find(index: Map<K, R>): R | undefined;
+}
+
+interface Indexer<R, K> {
+	index(row: R): K;
+}
+
+// R: Row type
+// V: Value type
+class Equals<R, V> implements Matcher<R>, Finder<V, R>, Indexer<R, V> {
+	readonly getter: (row: R) => V
+	readonly value: V;
+
+	constructor(getter: (row: R) => V, value: V) {
+		this.getter = getter;
+		this.value = value;
+	}
+
+	doesMatch(row: R): boolean {
+		return this.getter(row) === this.value;
+	}
+
+	find(index: Map<V, R>): R | undefined {
+		return index.get(this.value);
+	}
+
+	index(row: R): V {
+		return this.getter(row);
+	}
+}
+
+type Country = {
+	name: string,
+	population: number
+}
+
+const countries: Country[] = [
+	{name: "Australia", population: 24.6},
+	{name: "Canada", population: 36.54}
+]
+
+const byCountryName = new Equals<Country, string>((row) => row.name, "Australia");
+
+console.log(true, byCountryName.doesMatch(countries[0]));
+console.log(false, byCountryName.doesMatch(countries[1]));
+
+console.log("Australia", byCountryName.index(countries[0]));
+console.log("Canada", byCountryName.index(countries[1]));
+
+var index: Map<string, Country> = new Map();
+index.set("Australia", countries[0]);
+
+console.log(countries[0], byCountryName.find(index));
+
