02807b8fd595284fd5ec067041949222bc9a3b56
Logan Campbell <colin@logaan.net>
2020-01-18 00:41:05 +1100

    tests for And(Equals, Equals)

--- src/index.ts
+++ src/index.ts
@@ -54,6 +54,19 @@ class And<R, VL, VR> implements Indexable<[VL, VR], R> {
     }
 }
 
+// -------------------- Test suite --------------------
+// -------------------- Helpers --------------------
+
+function isEqual<A>(a1: A, a2: A): boolean {
+    return JSON.stringify(a1)==JSON.stringify(a2);
+}
+
+function test<A>(expected: A, actual: A) {
+    console.log(isEqual(expected, actual));
+}
+
+// -------------------- Data --------------------
+
 type Country = {
     name: string,
     population: number
@@ -64,6 +77,7 @@ const countries: Country[] = [
     {name: "Canada", population: 36.54}
 ]
 
+// -------------------- Equals --------------------
 const byCountryName = new Equals<Country, string>((row) => row.name, "Australia");
 
 console.log(true === byCountryName.doesMatch(countries[0]));
@@ -74,3 +88,14 @@ console.log("Canada" === byCountryName.index(countries[1]));
 
 console.log("Australia" === byCountryName.key());
 
+// -------------------- And --------------------
+const byCountryPopulation = new Equals<Country, number>((row) => row.population, 24.6);
+const byCountryNameAndPopn = new And(byCountryName, byCountryPopulation);
+
+test(true, byCountryNameAndPopn.doesMatch(countries[0]));
+test(false, byCountryNameAndPopn.doesMatch(countries[1]));
+
+test(["Australia", 24.6], byCountryNameAndPopn.index(countries[0]));
+test(["Canada", 36.54], byCountryNameAndPopn.index(countries[1]));
+
+test(['Australia', 24.6], byCountryNameAndPopn.key());
